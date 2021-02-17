b97d14def755274acccb930624cf406f7d571f3e
Mathew Seng <mathewseng@gmail.com>
2021-02-17 14:59:05 -0600

    Create Meeting testing spec
    This verifies the following:
    1) If a Meeting object can be createed
    2) The type of attributes to title, password1, and password2 are string
    3) Verify a Meeting object can contain actual data

--- Oblivion/src/app/models/meeting.model.spec.ts
+++ Oblivion/src/app/models/meeting.model.spec.ts
@@ -4,4 +4,26 @@ describe('Meeting', () => {
   it('should create an instance', () => {
     expect(new Meeting()).toBeTruthy();
   });
+
+  it('should have specific types in Meeting model', () => {
+    const meeting = new Meeting();
+    meeting.title = 'test';
+    meeting.password1 = 'test1';
+    meeting.password2 = 'test1';
+
+    expect(typeof(meeting.title)).toEqual('string');
+    expect(typeof(meeting.password1)).toEqual('string');
+    expect(typeof(meeting.password2)).toEqual('string');
+  });
+
+  it('should create an instance with data', () => {
+    const meeting = new Meeting();
+    meeting.title = 'test';
+    meeting.password1 = 'test1';
+    meeting.password2 = 'test1';
+
+    expect(meeting.title).toEqual('test');
+    expect(meeting.password1).toEqual('test1');
+    expect(meeting.password2).toEqual('test1');
+  });
 });
