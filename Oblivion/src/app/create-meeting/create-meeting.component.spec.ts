f6486f3969deaf8f92a61c8927781b31ef9566ec
Mathew Seng <mathewseng@gmail.com>
2021-02-17 15:09:14 -0600

    CreateMeeting Component testing
    This establishes the tests for the CreateMeeting component which entails:
    1) Creates a CreateMeeting Component
    2) Verifies Meeting title input and label
    3) Verifies Meeting password1 input, label and button
    4) Verifies Meeting password2 input, label and button
    5) Asserts an empty Meeting Model is created
    6) Asserts passwords are initially empty
    7) Verifies hide password button works
    8) Verify Create Meeting button exists and calls saveMeeting function
    9) Verify Cancel button exists and href

--- Oblivion/src/app/create-meeting/create-meeting.component.spec.ts
+++ Oblivion/src/app/create-meeting/create-meeting.component.spec.ts
@@ -1,5 +1,4 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import {ComponentFixture, fakeAsync, TestBed, tick} from '@angular/core/testing';
 import { CreateMeetingComponent } from './create-meeting.component';
 
 describe('CreateMeetingComponent', () => {
@@ -21,5 +20,81 @@ describe('CreateMeetingComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should contain a meeting name label and empty input', () => {
+    const label = fixture.debugElement.nativeElement.querySelector('#meeting_name_label');
+    expect(label.innerHTML).toBe('Enter Meeting Name');
+
+    const input = fixture.debugElement.nativeElement.querySelector('#meeting_name_input');
+    expect(input.innerHTML).toBe('');
+  });
+
+  it('should contain a meeting password label, empty input and hide button', () => {
+    const label = fixture.debugElement.nativeElement.querySelector('#meeting_password1_label');
+    expect(label.innerHTML).toBe('Enter Meeting Password');
+
+    const input = fixture.debugElement.nativeElement.querySelector('#meeting_password1_input');
+    expect(input.innerHTML).toBe('');
+
+    const button = fixture.debugElement.nativeElement.querySelector('#meeting_password1_hide_button');
+    expect(button).toBeTruthy();
+  });
+
+  it('should contain a meeting password confirmation label, empty input and hide button', () => {
+    const label = fixture.debugElement.nativeElement.querySelector('#meeting_password2_label');
+    expect(label.innerHTML).toBe('Confirm Meeting Password');
+
+    const input = fixture.debugElement.nativeElement.querySelector('#meeting_password2_input');
+    expect(input.innerHTML).toBe('');
+
+    const button = fixture.debugElement.nativeElement.querySelector('#meeting_password2_hide_button');
+    expect(button).toBeTruthy();
+  });
+
+  it('should contain an empty Meeting model', () => {
+    expect(component.meeting.title).toEqual('');
+    expect(component.meeting.id).toEqual('');
+    expect(component.meeting.password1).toEqual('');
+    expect(component.meeting.password2).toEqual('');
+  });
+
+  it('should have passwords initially hidden', () => {
+    expect(component.hide).toEqual(true);
+  });
+
+  it('should show passwords after hide button click (initially for password1)', () => {
+    const button = fixture.debugElement.nativeElement.querySelector('#meeting_password1_hide_button');
+    button.click();
+    expect(component.hide).toEqual(false);
+    button.click();
+    expect(component.hide).toEqual(true);
+  });
+
+  it('should show passwords after hide button click (initially for password2)', () => {
+    const button = fixture.debugElement.nativeElement.querySelector('#meeting_password2_hide_button');
+    button.click();
+    expect(component.hide).toEqual(false);
+    button.click();
+    expect(component.hide).toEqual(true);
+  });
+
+  it('should contain a create meeting button that calls saveMeeting', fakeAsync(() => {
+    spyOn(component, 'saveMeeting');
+
+    const button = fixture.debugElement.nativeElement.querySelector('#create_meeting_submit');
+    expect(button.innerHTML).toEqual('Create meeting!');
+
+    button.click();
+    tick();
+    expect(component.saveMeeting).toHaveBeenCalled();
+  }));
+
+  it('should contain an exit meeting button that should redirect to welcome page', () => {
+    const button = fixture.debugElement.nativeElement.querySelector('#create_meeting_exit');
+    expect(button.innerHTML).toEqual('Cancel');
+
+    const path = button.getAttribute('routerLink');
+    expect(path).toEqual('../welcome');
   });
 });
