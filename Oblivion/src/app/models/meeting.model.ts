fde2fc7bfc5d77103f656e7ee33be369a34b4253
Bailey Leuth <bailey-leuth@uiowa.edu>
2021-03-24 06:57:22 -0500

    Updated the meeting component a bit to make it more user friendly. Also changed the corresponding html pages

--- Oblivion/src/app/models/meeting.model.ts
+++ Oblivion/src/app/models/meeting.model.ts
@@ -7,11 +7,11 @@ export class Meeting {
 
   // TODO: Do we really need to send both passwords? Or can we just use the guards?
   public meetingType: MEETING_TYPE;
-  public password1?: string;
-  public password2?: string;
-  public title?: string;
-  //public join?: boolean;
-  public id?: any;
+  public password?: string;
+  public name?: string;
+  public meetingID?: any;
+  // public password1?: string;
+  // public password2?: string;
 
   constructor(meetingType: MEETING_TYPE) {
     this.meetingType = meetingType;
