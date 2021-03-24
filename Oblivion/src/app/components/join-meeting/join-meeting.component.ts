9b3b04fc781c886476d7819687825fe3a9c9a5d7
Bailey Leuth <bailey-leuth@uiowa.edu>
2021-03-24 02:35:40 -0500

    Updated the join-meeting component to use the WebsocketService in order to create the websocket. Also cleaned up the class

--- Oblivion/src/app/components/join-meeting/join-meeting.component.ts
+++ Oblivion/src/app/components/join-meeting/join-meeting.component.ts
@@ -1,41 +1,29 @@
-import { Component, OnInit } from '@angular/core';
-import {Meeting} from "../../models/meeting.model";
-import {CreateMeetingService} from "../../services/create-meeting.service";
+import {Component} from '@angular/core';
+import {Meeting, MEETING_TYPE} from "../../models/meeting.model";
+import {WebsocketService} from "../../services/websocket.service";
 
 @Component({
   selector: 'app-join-meeting',
   templateUrl: './join-meeting.component.html',
   styleUrls: ['./join-meeting.component.css']
 })
-export class JoinMeetingComponent implements OnInit {
+export class JoinMeetingComponent {
 
+  public meeting: Meeting;  // Stores the meeting creation information
+  public hide: Boolean;     // Indicates whether the password fields should be hidden
+  private webSocket: WebSocket;  // WebSocket connection to server
 
-  meeting: Meeting = {
-    id: '',
-    password1: '',
-    password2: '',
-    title: '',
-    join: true
-  };
-
-  hide = true;
-
-
-
-  constructor(private createMeetingService: CreateMeetingService) { }
-
-  ngOnInit(): void {
-
+  // Initializes the WebSocket from the WebsocketService and joins the meeting
+  constructor(websocketService: WebsocketService) {
+    this.webSocket = websocketService.getWebSocket();
+    this.meeting = new Meeting(MEETING_TYPE.JOIN);
+    this.hide = true;
   }
 
-  joinMeeting(): void {
-
-    console.log(this.meeting);
-    this.createMeetingService.connect();
-    this.createMeetingService.sendMessage(this.meeting);
-
-
+  // Sends the join meeting info to the server
+  public joinMeeting(): void {
+    console.log(JSON.stringify(this.meeting));
+    this.webSocket.send(JSON.stringify(this.meeting));
   }
-
 
 }
