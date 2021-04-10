910f9fc1aafe87971c11b71ce38a5cefba3906bd
Mathew Seng <mathewseng@gmail.com>
2021-04-09 19:14:29 -0500

    Change dev values to production values

--- Server/server.js
+++ Server/server.js
@@ -5,8 +5,8 @@ const https = require('https');
 const fs = require('fs');
 
 const credentials = {
-    key: fs.readFileSync('/home/mseng/server.key'),
-    cert: fs.readFileSync('/home/mseng/server.cert'),
+    key: fs.readFileSync(''),
+    cert: fs.readFileSync(''),
 }
 
 console.log(credentials);
