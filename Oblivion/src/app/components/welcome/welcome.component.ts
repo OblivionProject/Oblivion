02cc0caff08bebf8eb1a7ef5443c0936f735e85f
mseng10 <mathewseng@gmail.com>
2021-04-13 01:42:27 -0500

    Adjust styling for animation

--- Oblivion/src/app/components/welcome/welcome.component.ts
+++ Oblivion/src/app/components/welcome/welcome.component.ts
@@ -19,7 +19,7 @@ export class WelcomeComponent implements AfterViewInit {
     textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g,  "<span class='letter'>$&</span>");
 
     // @ts-ignores
-    anime.timeline({loop: true})
+    anime.timeline({loop: false})
       .add({
         targets: '.an-1 .letter',
         scale: [4, 1],
@@ -30,7 +30,7 @@ export class WelcomeComponent implements AfterViewInit {
         delay: (el: any, i: number) => 100 * i
       }).add({
       targets: '.an-1',
-      opacity: 0,
+      opacity: 1,
       duration: 1000,
       easing: 'easeOutExpo',
       delay: 1000
