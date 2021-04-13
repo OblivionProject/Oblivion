2e96c16befbbd45e4a7f82627b9ddc95c9017cfd
mseng10 <mathewseng@gmail.com>
2021-04-13 01:44:04 -0500

    Add form validation and display invalid password/meetingID

--- Oblivion/src/app/components/join-meeting/join-meeting.component.ts
+++ Oblivion/src/app/components/join-meeting/join-meeting.component.ts
@@ -2,6 +2,15 @@ import {Component} from '@angular/core';
 import {Meeting, MEETING_TYPE} from "../../models/meeting.model";
 import {WebsocketService} from "../../services/websocket.service";
 import { Router } from '@angular/router';
+import {FormBuilder, FormControl, FormGroup, FormGroupDirective, NgForm, Validators} from "@angular/forms";
+import {ErrorStateMatcher} from "@angular/material/core";
+
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
 
 @Component({
   selector: 'app-join-meeting',
@@ -13,13 +22,18 @@ export class JoinMeetingComponent {
   public meeting: Meeting;  // Stores the meeting creation information
   public hide: boolean;     // Indicates whether the password fields should be hidden
   private webSocket: WebSocket;  // WebSocket connection to server
+  joinMeetingForm: FormGroup;
+  timeout: any = null;
+  doneTyping = false;
+  matcher = new MyErrorStateMatcher();
 
   // Initializes the WebSocket from the WebsocketService and joins the meeting
-  constructor(websocketService: WebsocketService, private router: Router) {
+  constructor(websocketService: WebsocketService, private router: Router, private fb: FormBuilder) {
     this.webSocket = websocketService.getWebSocket();
     this.webSocket.onmessage = (message: MessageEvent) => this.receivedValidationFromServer(message);
     this.meeting = new Meeting(MEETING_TYPE.JOIN);
     this.hide = true;
+    this.joinMeetingForm = this.createJoinForm(fb);
   }
 
   // Sends the join meeting info to the server
@@ -29,17 +43,55 @@ export class JoinMeetingComponent {
     this.webSocket.send(JSON.stringify(this.meeting));
   }
 
+  createJoinForm(formBuilder: FormBuilder): FormGroup {
+    return formBuilder.group({
+      meetingID: new FormControl('', [Validators.required]),
+      password: new FormControl('', [])});
+  }
+
+  getMeetingIDErrorMessage(): string {
+    if (this.joinMeetingForm.contains('meetingID') && this.joinMeetingForm.controls.meetingID.hasError('required')){
+      return 'You must enter a meeting ID';
+    }
+    return 'Invalid ID';
+  }
+  getMeetingPasswordErrorMessage(): string {
+    return 'Invalid Password';
+  }
+
+  test(event: any): void{
+    this.doneTyping = false;
+    clearTimeout(this.timeout);
+    const $this = this;
+    this.timeout = setTimeout( () => {
+      if (event.keyCode !== 13) {
+        $this.displayIssues(event.target.value);
+      }
+    }, 1000);
+  }
+
   public receivedValidationFromServer(message: MessageEvent) {
     const signal = JSON.parse(message.data);
     console.log(signal);
     if (signal.error){
-      this.router.navigate(['welcome']);
+      if(signal.message == 'Invalid ID'){
+        // @ts-ignore
+        this.joinMeetingForm.get('meetingID').setErrors({ invalidID: signal.message});
+      }
+      else if(signal.message == 'Invalid Password'){
+        // @ts-ignore
+        this.joinMeetingForm.get('password').setErrors({ password: signal.message});
+      }
     }
     else if(signal.valid){
       this.meeting.check = false;
       this.webSocket.send(JSON.stringify(this.meeting));
       this.router.navigate(['meeting']);
     }
+  }
+
+  displayIssues(value: string): void {
+    this.doneTyping = true;
   }
 
 }
